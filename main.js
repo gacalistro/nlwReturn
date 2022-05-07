78139301f4cd45c5f9fdd303253376c27ce54e9d
Gabriel Calistro <gahcalistro@gmail.com>
2022-05-07 18:54:04 -0300

    section contact and footer + backToTopButton + fix loading function onScroll + custom colors

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
+window.addEventListener("scroll", onScroll);
+onScroll();
+
 function onScroll() {
+  showNavOnScroll();
+  showBackToTopButton();
+}
+
+function showNavOnScroll() {
   scrollY
     ? navigation.classList.add("scroll")
     : navigation.classList.remove("scroll");
+}
+
+function showBackToTopButton() {
+  if (scrollY > 400) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
 }
 
 function openMenu() {
